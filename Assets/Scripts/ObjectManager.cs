dacd192e39e4598123186fe5b8b225c94b145ed5
Mitchell <mmoscovics@yahoo.com>
2021-09-16 16:18:17 -0400

    [Update] object instantiate

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -8,11 +8,17 @@ using Photon.Realtime;
 
 public class ObjectManager : MonoBehaviour
 {
+    public Camera mainCam;
+    public GameObject cubePrefab;
+
     public void spawnObject(string prefab_name)
-    {   
-        Vector3 player_position = GetComponent<Camera>().transform.position;
+    {
+
+
+        Vector3 player_position = mainCam.transform.position;
         player_position.z += .5f;
-        Quaternion player_rotation = GetComponent<Camera>().transform.rotation;
+        Quaternion player_rotation = mainCam.transform.rotation;
+        Resources.Load(prefab_name);
         GameObject newAnchor = PhotonNetwork.Instantiate(prefab_name, player_position, player_rotation, 0);
     }
 }
